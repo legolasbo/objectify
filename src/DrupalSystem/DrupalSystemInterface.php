4fff112880bc88ccf2700e37a1aebf9d6082e7ac
Chris Jansen <chrisjansen@Chriss-MacBook-Pro.local>
2017-01-05 12:04:50 +0100

    Complete the docblocks.

--- src/DrupalSystem/DrupalSystemInterface.php
+++ src/DrupalSystem/DrupalSystemInterface.php
@@ -17,21 +17,36 @@ interface DrupalSystemInterface extends XautoloadDrupalSystemInterface {
 
   /**
    * @see drupal_static()
+   *
+   * @param string $name
+   * @param mixed $default_value
+   * @param bool $reset
+   * @return mixed
    */
   public function &drupalStatic($name, $default_value = NULL, $reset = FALSE);
 
   /**
    * @see drupal_get_path()
+   *
+   * @param string $type
+   * @param string $name
+   * @return string
    */
   public function drupalGetPath($type, $name);
 
   /**
    * @see render()
+   *
+   * @param $element
+   * @return string
    */
   public function render(&$element);
 
   /**
    * @see drupal_render()
+   *
+   * @param $elements
+   * @return string
    */
   public function drupalRender(&$elements);
 
@@ -51,41 +66,78 @@ interface DrupalSystemInterface extends XautoloadDrupalSystemInterface {
 
   /**
    * @see drupal_goto()
+   *
+   * @param string $path
+   * @param array $options
+   * @param int $http_response_code
    */
   public function drupalGoto($path = '', array $options = [], $http_response_code = 302);
 
   /**
    * @see drupal_set_message()
+   *
+   * @param string $message
+   * @param string $type
+   * @param bool $repeat
+   * @return array|null
    */
   public function drupalSetMessage($message = NULL, $type = 'status', $repeat = TRUE);
 
   /**
    * @see module_invoke_all()
+   *
+   * @param string $hook
+   * @param mixed $parameter
+   * @param mixed $_
+   * @return array
    */
   public function moduleInvokeAll($hook, $parameter = NULL, $_ = NULL);
 
   /**
    * @see module_invoke()
+   *
+   * @param string $module
+   * @param string $hook
+   * @return mixed
    */
   public function moduleInvoke($module, $hook);
 
   /**
    * @see drupal_alter().
+   *
+   * @param string $type
+   * @param $data
+   * @param $context1
+   * @param $context2
+   * @param $context3
    */
   public function drupalAlter($type, &$data, &$context1 = NULL, &$context2 = NULL, &$context3 = NULL);
 
   /**
    * @see format_date()
+   *
+   * @param $timestamp
+   * @param string $type
+   * @param string $format
+   * @param null $timezone
+   * @param null $langcode
+   * @return string
    */
   public function formatDate($timestamp, $type = 'medium', $format = '', $timezone = NULL, $langcode = NULL);
 
   /**
    * @see drupal_get_breadcrumb()
+   *
+   * @return array
    */
   public function drupalGetBreadcrumb();
 
   /**
    * @see drupal_get_form()
+   *
+   * @param $form_id
+   * @param array $arguments
+   * @return array
    */
   public function drupalGetForm($form_id, array $arguments = []);
 
@@ -105,11 +157,17 @@ interface DrupalSystemInterface extends XautoloadDrupalSystemInterface {
    *   $this->system->callFunction('drupal_set_title', [$this->t('A title for a page')]);
    * Passing a variable by reference:
    *   $system->callFunction('another_function', [$var1, &var2_by_reference]);
+   *
+   * @param string $func
+   * @param array $args
+   * @return mixed
    */
   public function callFunction($func, array $args = []);
 
   /**
    * @see current_path()
+   *
+   * @return string
    */
   public function currentPath();
 
