df1521354ad821287924dc158b77f1b4f8271c3f
Chris Jansen <chrisjansen@Chriss-MacBook-Pro.local>
2017-01-05 12:10:39 +0100

    Introduce FormElementBuilder

---
+++ form/src/Form/FormElementBuilder.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\objectify_form\Form;
+
+class FormElementBuilder {
+
+  /**
+   * @param string|array $class
+   * @return array
+   */
+  public static function containerElementWithClass($class) {
+    return self::addClassToElement($class, self::containerElement());
+  }
+
+  /**
+   * @param string|array $class
+   * @param array $element
+   * @return array
+   */
+  private static function addClassToElement($class, array $element) {
+    $classes = (array) $class;
+
+    foreach ($classes as $class) {
+      $element['#attributes']['class'][] = $class;
+    }
+
+    return $element;
+  }
+
+  /**
+   * @return array
+   */
+  public static function containerElement() {
+    return [
+      '#type' => 'container'
+    ];
+  }
+
+  /**
+   * @param string $title
+   * @param mixed $defaultValue
+   * @param bool $required
+   * @return mixed
+   */
+  public static function textFieldWithDefaultValue($title, $defaultValue, $required = FALSE) {
+    return self::addDefaultValueToElement($defaultValue, self::textField($title, $required));
+  }
+
+  /**
+   * @param mixed $defaultValue
+   * @param array $element
+   * @return array
+   */
+  public static function addDefaultValueToElement($defaultValue, array $element) {
+    $element['#default_value'] = $defaultValue;
+
+    return $element;
+  }
+
+  /**
+   * @param string $title
+   * @param bool $required
+   * @return array
+   */
+  public static function textField($title, $required = FALSE) {
+    return self::simpleField('textfield', $title, $required);
+  }
+
+  /**
+   * @param $type
+   * @param $title
+   * @param bool $required
+   * @return array
+   */
+  public static function simpleField($type, $title, $required = FALSE) {
+    return [
+      '#type' => $type,
+      '#title' => $title,
+      '#required' => $required,
+    ];
+  }
+
+  /**
+   * @param mixed $value
+   * @return array
+   */
+  public static function valueElement($value) {
+    return [
+      '#type' => 'value',
+      '#value' => $value,
+    ];
+  }
+
+  /**
+   * @param string $title
+   * @param mixed $defaultValue
+   * @param bool $required
+   * @return array
+   */
+  public static function dateFieldWithDefaultValue($title, $defaultValue, $required = FALSE) {
+    return self::addDefaultValueToElement($defaultValue, self::dateField($title, $required));
+  }
+
+  /**
+   * @param string $title
+   * @param bool $required
+   * @return array
+   */
+  public static function dateField($title, $required = FALSE) {
+    return self::simpleField('date', $title, $required);
+  }
+}
